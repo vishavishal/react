e7e3d207854e81ca5bd46c7a9444268515006626
vishavishal <vishavishal@gmail.com>
2020-09-18 08:33:31 +0530

    Updates

---
+++ precache-manifest.c2402cc64d045d1f06e85e86aa7b741c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1a5ccdf1dc2c6788db036aa2120314c",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "6b20bccc504d2bd071f4",
+    "url": "/react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "be48d7b5330c6f76777d",
+    "url": "/react/static/js/2.69158a15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.69158a15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b20bccc504d2bd071f4",
+    "url": "/react/static/js/main.7a957a78.chunk.js"
+  },
+  {
+    "revision": "bffc9fce660541ccc316",
+    "url": "/react/static/js/runtime-main.e5222803.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
